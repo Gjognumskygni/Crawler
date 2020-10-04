d058aa38ac6d0eb01495f6690729fc4d05b57045
TeiturMcSwagger <Teiturargeseloy@hotmail.com>
2020-10-04 20:59:30 +0200

    Proposals

--- crawler.py
+++ crawler.py
@@ -1,12 +1,33 @@
+from proposal import Process, Proposal
 from bs4 import BeautifulSoup
-import requests
 from vote import Vote
-from proposal import Proposer
+import requests
+import time
+
+def printProposal(proposalObj: Proposal):
+    print("Title:", proposalObj.title)
+    print("Proposers:", proposalObj.proposers)
+
+def isProposal(soup: BeautifulSoup):
+    title = Proposal.getTitle(soup)
+    if "Uppskot til løgtingslóg" in title:
+        return True
+    return False
+
+def scrapeProposal(soup: BeautifulSoup):
+    proposalObj = Proposal.createProposalObj(soup)
+    printProposal(proposalObj)
+    
+def makeRequest(url: str):
+    time.sleep(2)
+    result = requests.get(url)
+    src = result.content
+    return BeautifulSoup(src, 'lxml')
 
 def crawl():
-    result = requests.get("https://www.logting.fo/casenormal/viewState.gebs?caseState.id=28251&menuChanged=16")
-    src = result.content
-    soup = BeautifulSoup(src, 'lxml')
-    Proposer.createProposerObj(soup)
+    soup = makeRequest("https://www.logting.fo/casenormal/view.gebs?menuChanged=16&type=0&caseNormal.id=4708")
+    if isProposal(soup) == True:
+        Process.getProcess(soup)
+        ''' scrapeProposal(soup) '''
     """ Vote.createVoteObj(soup) """
     
--- proposal.py
+++ proposal.py
@@ -1,3 +1,4 @@
+from crawler import makeRequest
 from bs4 import BeautifulSoup
 from typing import List
 from vote import Vote
@@ -6,20 +7,32 @@ class Process():
     def __init__(self, number: int, vote: Vote):
         self.number = number
         self.vote = vote
-
-class Proposer():
-    def __init__(self, name: str):
-        self.name = name
     
     @staticmethod
-    def createProposerObj(soup: BeautifulSoup):
-        return Proposer(soup.find('span', class_='green').get_text())
+    def getVotes(soup: BeautifulSoup):
+        pass
+
+    @staticmethod
+    def getProcess(soup: BeautifulSoup):
+        process_list: List[Process] = []
+        for a in soup.find_all('a', class_="txt_type"):
+            if "2. viðgerð" in a.get_text() or "3. viðgerð" in a.get_text():
+                if len(process_list) == 0:
+                    newSoup = makeRequest("https://www.logting.fo/" + a['href'])
+                    process_list.append(Process(1,Process.getVotes(newSoup)))
+                else:
+                    newSoup = makeRequest("https://www.logting.fo/" + a['href'])
+                    process_list.append(Process(2,Process.getVotes(newSoup)))
 
 class Proposal():
-    def __init__(self, title: str):
+    def __init__(self, title: str, proposers: str):
         self.title = title
-        self.proposers: List[Proposer] = []
+        self.proposers = proposers
         self.processes: List[Process] = []
+
+    @staticmethod
+    def getProposer(soup: BeautifulSoup):
+        return soup.find('span', class_='green').get_text()
 
     @staticmethod
     def getTitle(soup: BeautifulSoup):
@@ -27,4 +40,4 @@ class Proposal():
     
     @staticmethod
     def createProposalObj(soup: BeautifulSoup):
-        return Proposal(Proposal.getTitle(soup))
+        return Proposal(Proposal.getTitle(soup), Proposal.getProposer(soup))
